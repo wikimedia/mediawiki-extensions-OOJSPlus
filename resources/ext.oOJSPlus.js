7e7f418e8707d509807f0d1bac1d695e2943e2c0
rvogel <vogel@hallowelt.biz>
2020-02-25 18:00:30 +0100

    Implement basic `OOJSPlus.define` functionality
    Change-Id: I8682ddf03875d61d269ed8e52cb0b59c8710ff27

--- resources/ext.oOJSPlus.js
+++ resources/ext.oOJSPlus.js
@@ -1,54 +1,74 @@
 ( function( mw, $, undefined ) {
 
-	function _makeContructorFromString( className, constructorFunction ) {
-
+	function _makeConstructorFromString( className, constructor ) {
+		var baseObject = window;
+		var parts = className.split( '.' );
+		var part = parts.shift();
+		baseObject[part] = baseObject[part] || {};
+		baseObject = baseObject[part];
+		while( parts.length > 1 ) {
+			part = parts.shift();
+			baseObject[part] = baseObject[part] || {};
+			baseObject = baseObject[part];
+		}
+		var lastPart = parts.shift();
+		if( baseObject[lastPart] instanceof Function === false ) {
+			baseObject[lastPart] = constructor;
+		}
+		return baseObject[lastPart];
 	}
 
 	var definitionSpecialFields = [ 'extend', 'requires', 'constructor', 'static', 'mixins' ];
-	function _define( className, definition ) {
-		throw new Error( 'NOT WORKING' );
 
+	function _define( className, definition ) {
+		var i = 0;
 		//TODO: evaluate 'requires', load by dedicated RL module and make actual definition async
 
-		var _constructor = _makeContructorFromString( className, definition.contructor || function() {} );
+		var _constructor = _makeConstructorFromString( className, definition.constructor || function() {} );
 
 		if( definition.extend ) {
-			var _baseConstructor = _makeContructorFromString( definition.extend );
+			var _baseConstructor = _makeConstructorFromString( definition.extend );
 			OO.inheritClass( _constructor, _baseConstructor );
+
+			//For some reason the "static" member is not inherited
+			_constructor.static = _baseConstructor.static;
 		}
 		else {
 			OO.initClass( _constructor );
 		}
 
 		if( definition.mixins ) {
-			for( var i = 0; i < definition.mixins.length; i++ ) {
-				var _mixinConstructor = _makeContructorFromString( definition.mixins[i] );
+			for( i = 0; i < definition.mixins.length; i++ ) {
+				var _mixinConstructor = _makeConstructorFromString( definition.mixins[i] );
 				OO.mixinClass( _constructor, _mixinConstructor );
 			}
 		}
 
 		if( definition.static ) {
-			for( var fieldName in definition.static ) {
-				_constructor.static[fieldName] = definition.static[fieldName];
+			_constructor.static = _constructor.static || {};
+			for( var staticFieldName in definition.static ) {
+				if( staticFieldName.indexOf( '+' ) === 0 ) {
+					var unprefixedStaticFieldName = staticFieldName.substring( 1 );
+					for( i = 0; i < definition.static[staticFieldName].length; i++ ) {
+						_constructor.static[unprefixedStaticFieldName].push( definition.static[staticFieldName][i] );
+					}
+				}
+				else {
+					_constructor.static[staticFieldName] = definition.static[staticFieldName];
+				}
 			}
 		}
 
 		for( var fieldName in definition ) {
-			if( $.inArray( fieldName, definitionSpecialFields ) ) {
+			if( $.inArray( fieldName, definitionSpecialFields ) !== -1 ) {
 				continue;
 			}
 			_constructor.prototype[fieldName] = definition[fieldName];
 		}
 	}
 
-	function _require( className, callback ) {
-		throw new Error( 'NOT WORKING' );
-		//TBD
-	}
-
 	OOJSPlus = {
 		define: _define,
-		require: _require,
 		ui: {
 			data: {
 				column: {},
