ddd70127a506917c0c93579efca23417babb1df3
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-10-17 09:30:03 +0200

    Add ColorPickerStandaloneWidget
    [all]
    
    ERM 21311
    
    Change-Id: Id4cc481c1809cab9c9f967049fbc4745e01b4b4c

---
+++ resources/ui/widget/ColorPickerStandalone.js
@@ -0,0 +1,15 @@
+( function( mw, $ ) {
+	OOJSPlus.ui.widget.ColorPickerStandaloneWidget = function( cfg ) {
+		cfg = cfg || {};
+
+		OOJSPlus.ui.widget.ColorPickerStandaloneWidget.parent.call( this, cfg );
+
+		this.$element.addClass( 'oojs-plus-color-picker-standalone' );
+	};
+
+	OO.inheritClass( OOJSPlus.ui.widget.ColorPickerStandaloneWidget, OOJSPlus.ui.widget.ColorPickerEmbeddable );
+
+	OOJSPlus.ui.widget.ColorPickerStandaloneWidget.static.tagName = 'div';
+
+
+} ) ( mediaWiki, jQuery );
