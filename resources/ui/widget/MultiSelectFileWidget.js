6b38ddf744b69d0a38d3fa8bbd6afee7bc515636
miriam schlindwein <schlindwein@hallowelt.com>
2022-02-09 11:33:42 +0100

    Add MultiSelectFileWidget
    ERM27559
    
    [4.x]
    
    Change-Id: I33854c52c4f486d0bc97d198bf9d0b57e872f9a8

---
+++ resources/ui/widget/MultiSelectFileWidget.js
@@ -0,0 +1,34 @@
+( function () {
+
+	OOJSPlus.ui.widget.MultiSelectFileWidget = function( config ) {
+		config = config || {};
+
+		this.uploadFiles = [];
+		// Parent constructor
+		OOJSPlus.ui.widget.MultiSelectFileWidget.parent.call( this, $.extend( {}, config, {} ) );
+	}
+
+	OO.inheritClass( OOJSPlus.ui.widget.MultiSelectFileWidget, OO.ui.SelectFileWidget );
+	OO.mixinClass( OOJSPlus.ui.widget.MultiSelectFileWidget, OO.ui.mixin.PendingElement );
+
+	OOJSPlus.ui.widget.MultiSelectFileWidget.prototype.setValue = function ( files ) {
+		if ( files === undefined ) {
+			return;
+		}
+
+		if ( files ) {
+			for ( var key in files ) {
+				this.uploadFiles.push( files[ key ] );
+			}
+		} else {
+			this.uploadFiles = [];
+		}
+
+		this.emit( 'change', this.uploadFiles );
+	}
+
+	OOJSPlus.ui.widget.MultiSelectFileWidget.prototype.getValue = function() {
+		return this.uploadFiles;
+	}
+
+}() );
