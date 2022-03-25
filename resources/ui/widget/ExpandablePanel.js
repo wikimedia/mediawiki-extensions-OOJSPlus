2d0f5d54e0d8aa36d3b6d1de5ea66ae97307794a
d.savuljesku <d.savuljesku@gmail.com>
2022-03-25 08:23:40 +0100

    Add ExpandablePanelWidget
    [4.2]
    
    ERM 767796
    
    Change-Id: I28dff3a5d1585c1e9a7d8f0e2a58fa4d7244eee2

---
+++ resources/ui/widget/ExpandablePanel.js
@@ -0,0 +1,35 @@
+( function () {
+	OOJSPlus.ui.widget.ExpandablePanel = function( config ) {
+		config = config || {};
+		this.$cnt = config.$content || '';
+		delete( config.$content );
+		// Parent constructor
+		OOJSPlus.ui.widget.ExpandablePanel.parent.call( this, $.extend( {}, config, {} ) );
+		this.collapsed = config.collapsed || false;
+		this.button = new OO.ui.ButtonWidget( {
+			label: config.label || '',
+			icon: config.icon  || '',
+			framed: false,
+			indicator: this.collapsed ? 'down' : 'up',
+			classes: [ 'expander-button' ]
+		} );
+		this.button.connect( this, {
+			click: function() {
+				this.collapsed = !this.collapsed;
+				this.$area.toggle();
+				this.button.setIndicator( this.collapsed ? 'down' : 'up' );
+				this.emit( 'stateChange', this.collapsed );
+			}
+		} );
+		this.$area = $( '<div>' ).html( this.$cnt );
+		if ( this.collapsed ) {
+			this.$area.hide();
+		}
+
+		this.$element.append( this.button.$element, this.$area );
+
+		this.$element.addClass( 'oojsplus-ui-expandable-panel' );
+	};
+
+	OO.inheritClass( OOJSPlus.ui.widget.ExpandablePanel, OO.ui.PanelLayout );
+}() );
