082aae46af05f06d1d3878007053c83baf7eeaeb
miriam schlindwein <schlindwein@hallowelt.com>
2022-04-26 14:32:46 +0200

    Allow multiple values in a cell
    ERM27833
    
    [4.x]
    
    Change-Id: I246be4fd19c627c75b10abd091c7dac326e1fcc1

--- resources/ui/data/column/Url.js
+++ resources/ui/data/column/Url.js
@@ -12,7 +12,7 @@
 
 	OO.inheritClass( OOJSPlus.ui.data.column.Url, OOJSPlus.ui.data.column.Column );
 
-	OOJSPlus.ui.data.column.Url.prototype.renderCell = function( value, row ) {
+	OOJSPlus.ui.data.column.Url.prototype.renderCell = function ( value, row ) {
 		var $cell = $( '<td>' ).addClass( 'oojsplus-data-gridWidget-cell' );
 		$cell.attr( 'data-column', this.id );
 		$cell.attr( 'data-value', value );
@@ -23,13 +23,32 @@
 
 	OOJSPlus.ui.data.column.Url.prototype.getViewControls = function( value, row ) {
 		var url = row[this.urlProperty];
-		var linkButton = new OO.ui.ButtonWidget( {
+		if ( !Array.isArray( value ) ) {
+			value = [ value ];
+			if ( !Array.isArray( url ) ) {
+				url = [ url ];
+			}
+		}
+
+		if ( url.length === 1 ){
+			return this.getViewSingleControl( value[0], url[0] );
+		}
+
+		var items = [];
+		for ( var i = 0; i < value.length; i++ ) {
+			items.push( this.getViewSingleControl( value[i], url[i] ) );
+		}
+
+		return new OO.ui.FieldsetLayout( { items: items } );
+	};
+
+	OOJSPlus.ui.data.column.Url.prototype.getViewSingleControl = function ( label, url ) {
+		return new OO.ui.ButtonWidget( {
 			href: url,
-			label: value,
+			label: label,
 			framed: false,
 			target: this.urlExternal
 		} );
-		return linkButton;
 	};
 
 	OOJSPlus.ui.data.column.Url.prototype.sort = function( a, b ) {
