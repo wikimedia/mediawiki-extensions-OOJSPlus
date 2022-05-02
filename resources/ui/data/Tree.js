a521f2ec235ee2104069472457e80903196bc692
d.savuljesku <d.savuljesku@gmail.com>
2022-05-02 16:24:22 +0200

    Improve tree widget
    Change-Id: I90133b390398b85a2c77aafd6ec4cd70ffeca949

--- resources/ui/data/Tree.js
+++ resources/ui/data/Tree.js
@@ -58,17 +58,7 @@
 			) {
 				isLeaf = false;
 			}
-			var widget = new this.itemClass( {
-				name: item.name,
-				type: item.type || '',
-				icon: item.icon || '',
-				label: item.label || '',
-				indicator: item.indicator || '',
-				level: lvl,
-				isLeaf: isLeaf,
-				childrenCount: item.hasOwnProperty( 'items' ) ? item.items.length : 0,
-				tree: this
-			} );
+			var widget = this.createItemWidget( item, lvl, isLeaf );
 
 			widget.connect( this, {
 				selected: function( item ) {
@@ -85,6 +75,20 @@
 
 			this.build( item.items || [], lvl + 1, widget );
 		}
+	};
+
+	OOJSPlus.ui.data.Tree.prototype.createItemWidget = function( item, lvl, isLeaf ) {
+		return new this.itemClass( {
+			name: item.name,
+			type: item.type || '',
+			icon: item.icon || '',
+			label: item.label || '',
+			indicator: item.indicator || '',
+			level: lvl,
+			isLeaf: isLeaf,
+			childrenCount: item.hasOwnProperty( 'items' ) ? item.items.length : 0,
+			tree: this
+		} );
 	};
 
 	/** Generate HTML */
@@ -358,6 +362,7 @@
 		}
 		this.selectedItem = this.getItem( item.getName() );
 		this.selectedItem.widget.$element.addClass( 'item-selected' );
+		this.emit( 'itemSelected', item );
 	};
 
 	OOJSPlus.ui.data.Tree.prototype.getOrderedNames = function() {
