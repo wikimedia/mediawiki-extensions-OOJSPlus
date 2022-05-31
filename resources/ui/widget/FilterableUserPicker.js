d693d22e86d2506c6fef176e10badb05baef57ab
d.savuljesku <d.savuljesku@gmail.com>
2022-05-31 15:03:07 +0200

    Add expanded UserFilter widget
    [4.1.x]
    
    ERM 28736
    
    Change-Id: I54d8d18ac266e3e230caa4dfd79181329054988e

---
+++ resources/ui/widget/FilterableUserPicker.js
@@ -0,0 +1,38 @@
+OOJSPlus.ui.widget.FilterableUserPicker = function( config ) {
+	// Parent constructor
+	OOJSPlus.ui.widget.FilterableUserPicker.parent.call( this, $.extend( {}, config, {} ) );
+
+	this.rights = config.rights || null;
+	this.groups = config.groups || null;
+	this.excludeGroups = config.excludeGroups || null;
+};
+
+OO.inheritClass( OOJSPlus.ui.widget.FilterableUserPicker, mw.widgets.UserInputWidget );
+
+
+/**
+ * @inheritdoc
+ */
+OOJSPlus.ui.widget.FilterableUserPicker.prototype.getLookupRequest = function () {
+	var inputValue = this.value;
+
+	var data = {
+		action: 'query',
+		list: 'allusers',
+		// Prefix of list=allusers is case sensitive. Normalise first
+		// character to uppercase so that "fo" may yield "Foo".
+		auprefix: inputValue[ 0 ].toUpperCase() + inputValue.slice( 1 ),
+		aulimit: this.limit
+	};
+	if ( this.rights ) {
+		data.aurights = this.rights.join( '|' );
+	}
+	if ( this.groups ) {
+		data.augroup = this.groups.join( '|' );
+	}
+	if ( this.excludeGroups ) {
+		data.auexcludegroup = this.excludeGroups.join( '|' );
+	}
+
+	return new mw.Api().get( data );
+};
