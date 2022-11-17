6cc87a51c7b8428a3c5947cc89aedbdf9e0184a3
d.savuljesku <d.savuljesku@gmail.com>
2022-11-17 16:25:35 +0100

    Add new SelectButton widget
    [4.3]
    
    ERM30598
    
    Change-Id: I62b48efe50d8ed1d7f150e2db44a5e3b29a29e8f

---
+++ resources/ui/widget/MenuOptionWithDescription.js
@@ -0,0 +1,15 @@
+( function( mw, $ ) {
+	OOJSPlus.ui.widget.MenuOptionWithDescription = function( cfg ) {
+		cfg = cfg || {};
+		OOJSPlus.ui.widget.MenuOptionWithDescription.parent.call( this, cfg );
+		this.$element.addClass( 'oojs-plus-menu-option-with-description' );
+		if ( cfg.description ) {
+			this.$element.append( $( '<span>' )
+			.addClass( 'oojs-plus-menu-option-with-description-description' )
+			.text( cfg.description ) );
+		}
+	};
+
+	OO.inheritClass( OOJSPlus.ui.widget.MenuOptionWithDescription, OO.ui.MenuOptionWidget );
+
+} ) ( mediaWiki, jQuery );
