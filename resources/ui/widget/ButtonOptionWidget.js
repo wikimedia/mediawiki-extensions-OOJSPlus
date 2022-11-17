bd9f311fc959e91791a4bd9a702bc548659b9fab
d.savuljesku <d.savuljesku@gmail.com>
2022-11-17 16:25:35 +0100

    Add new SelectButton widget
    [4.3]
    
    ERM30598
    
    Change-Id: I62b48efe50d8ed1d7f150e2db44a5e3b29a29e8f
    (cherry picked from commit 6cc87a51c7b8428a3c5947cc89aedbdf9e0184a3)
    (cherry picked from commit 5c5c19e789136a51de284d4e6901101a87f5e6bb)

---
+++ resources/ui/widget/ButtonOptionWidget.js
@@ -0,0 +1,21 @@
+OOJSPlus.ui.widget.ButtonOptionWidget = function( cfg ) {
+	cfg = cfg || {};
+	cfg.framed = false;
+	OOJSPlus.ui.widget.ButtonOptionWidget.parent.call( this, cfg );
+	this.description = cfg.description || '';
+
+	this.$element.addClass( 'oojsplus-ui-widget-buttonoptionwidget' );
+
+	this.$element.append( new OO.ui.IconWidget( {
+		icon: 'check',
+		classes: [ 'oojsplus-ui-widget-buttonoptionwidget-check' ]
+	} ).$element );
+	if ( cfg.description ) {
+		this.$element.find( 'a' ).append( new OO.ui.LabelWidget( {
+			label: cfg.description,
+			classes: [ 'oojsplus-ui-widget-buttonoptionwidget-desc' ]
+		} ).$element );
+	}
+};
+
+OO.inheritClass( OOJSPlus.ui.widget.ButtonOptionWidget, OO.ui.ButtonOptionWidget );
