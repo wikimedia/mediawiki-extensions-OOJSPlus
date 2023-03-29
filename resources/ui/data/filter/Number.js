a59c8838bc0af6cf3fe9240fb137ecb56991b69c
d.savuljesku <d.savuljesku@gmail.com>
2023-03-29 12:10:20 +0200

    Add grid date filter + expandable rows
    - Filter `type: date` is now supported
    - If a grid row gets too long, it will be cut-off, but now can be expanded
    by clicking on `...`
    
    [4.2.x]
    
    ERM 32107
    
    Change-Id: I649e08ae835df05d83a5b17f65d1d4b5d76f101c
    (cherry picked from commit 6bc97b048241d28c32c6c0e38bf2b7efaeab4d98)

--- resources/ui/data/filter/Number.js
+++ resources/ui/data/filter/Number.js
@@ -7,6 +7,26 @@ OOJSPlus.ui.data.filter.Number = function ( cfg ) {
 OO.inheritClass( OOJSPlus.ui.data.filter.Number, OOJSPlus.ui.data.filter.Filter );
 
 OOJSPlus.ui.data.filter.Number.prototype.getLayout = function() {
+	this.makeOperatorWidget();
+	this.input = new OO.ui.NumberInputWidget( { step: 1 } );
+	this.input.connect( this, {
+		change: 'changeValue'
+	} );
+
+	return new OO.ui.FieldsetLayout( { items: [
+			new OO.ui.FieldLayout( this.operatorWidget, {
+				label: mw.message( 'oojsplus-data-grid-filter-operator' ).text(),
+				align: 'left'
+			} ),
+			new OO.ui.FieldLayout( this.input, {
+				label: mw.message( 'oojsplus-data-grid-filter-number-value' ).text(),
+				align: 'left'
+			} )
+		]
+	} );
+};
+
+OOJSPlus.ui.data.filter.Number.prototype.makeOperatorWidget = function() {
 	this.operatorWidget = new OO.ui.ButtonSelectWidget( {
 		items: [
 			new OO.ui.ButtonOptionWidget( {
@@ -26,22 +46,6 @@ OOJSPlus.ui.data.filter.Number.prototype.getLayout = function() {
 	this.operatorWidget.selectItemByData( this.operator );
 	this.operatorWidget.connect( this, {
 		select: 'changeOperator'
-	} );
-	this.input = new OO.ui.NumberInputWidget( { step: 1 } );
-	this.input.connect( this, {
-		change: 'changeValue'
-	} );
-
-	return new OO.ui.FieldsetLayout( { items: [
-			new OO.ui.FieldLayout( this.operatorWidget, {
-				label: mw.message( 'oojsplus-data-grid-filter-number-operator' ).text(),
-				align: 'left'
-			} ),
-			new OO.ui.FieldLayout( this.input, {
-				label: mw.message( 'oojsplus-data-grid-filter-number-value' ).text(),
-				align: 'left'
-			} )
-		]
 	} );
 };
 
