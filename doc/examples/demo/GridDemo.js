b34fe69e742eaf59c51d64d4195af9aca8e74bd4
d.savuljesku <d.savuljesku@gmail.com>
2023-05-05 16:30:36 +0200

    Allow grid grouping
    [4.x]
    
    ERM 32522
    
    Change-Id: I3cd0e12a90d0037c850d5b087b774ba0d00ca55a
    (cherry picked from commit 593e95884c7cfa6aff54436ad7dfa9963954f0bc)

--- doc/examples/demo/GridDemo.js
+++ doc/examples/demo/GridDemo.js
@@ -1,5 +1,24 @@
 // Local store
 var up_url = 'http://url-to-user';
+
+/**
+ * {
+ *     autoload: true, // Whether to autoload the store, or require explicit .load() call
+ *     data: [], // Array of objects to load (not with RemoteStore)
+ *     query: 'string', // Query to send to the server or locally filter specified {data}
+ *     remoteFilter: true | false, // Whether to filter remotely or locally
+ *     remoteSort: true | false, // Whether to sort remotely or locally
+ *     pageSize: 25, // Number of items per page
+ *     filter: { array_of_filters },
+ *     sorter: { array_of_sorters },
+ *     groupField: 'string' // Field to group by. If specified, store is responsible for properly sorting by groupField
+ *     // RemoteStore (Action API)
+ *     action: 'string', // Action API action name
+ *     // RemoteRestStore (REST API)
+ *     path: 'string', // REST API path (to be appended to {wiki/rest.php )
+ * }
+ * @type {OOJSPlus.ui.data.store.Store}
+ */
 var store = new OOJSPlus.ui.data.store.Store( {
 	data: [
 		{ firstName: "John", lastName: "Doe", address: "Main street 22", userpage: "JD1", up_url: up_url, active: true },
@@ -50,12 +69,17 @@ var store = new OOJSPlus.ui.data.store.Store( {
  *     noHeader: true|false // Hide header if true. Default to false
  *     pageSize: {number}, // Default to 10, also affected by the store's pageSize
  *     columns: { {definition of the columns}  }, // See Column.js
+ *     unspecifiedGroupHeader: 'No group selected', // If store has a groupField, this will be the header for the unspecified group
  *     store: {OOJSPlus.ui.data.store.Store}, // See Store.js
  *     data: {Array}, // Data to use if no store is specified (only evaluated if no store is specified)
  *     paginator: {Instance of OOJSPlus.ui.data.grid.Paginator}|null. Nul for no pagination. If not specified, a default paginator will be used
  *     toolbar: {Instance of OOJSPlus.ui.data.grid.Toolbar}|null. Null for no toolbar. If not specified, a default toolbar will be used
  *     tools: {Array of OO.ui.ButtonWidget or subclasses of it (eg. OO.ui.PopupButtonWidget)}. Tools to add to the toolbar
  * }
+ *
+ * GROUPING:
+ * If store has `groupField` specified, grid will automatically group by that field. Store is responsible for providing
+ * grouped items in order (sorted)
  * @type {OOJSPlus.ui.data.GridWidget}
  */
 var grid = new OOJSPlus.ui.data.GridWidget( {
