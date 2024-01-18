f7bffe7bba21191fd4d6ec5ae0a6d078f6e4d824
d.savuljesku <d.savuljesku@gmail.com>
2024-01-18 15:28:08 +0100

    Improvements on data grid
    [4.4.x]
    
    ERM35304
    
    Change-Id: I43e184667958b00f3074cb9d813bd1ab0a057315

--- resources/ui/data/column/Text.js
+++ resources/ui/data/column/Text.js
@@ -1,12 +1,16 @@
 OOJSPlus.ui.data.column.Text = function ( cfg ) {
 	OOJSPlus.ui.data.column.Text.parent.call( this, cfg );
 
+	this.isHtml = cfg.html || false;
 	this.$element.addClass( 'text-column' );
 };
 
 OO.inheritClass( OOJSPlus.ui.data.column.Text, OOJSPlus.ui.data.column.Column );
 
 OOJSPlus.ui.data.column.Text.prototype.renderCell = function( value, row ) {
+	if ( this.isHtml && typeof value === 'string' && !this.valueParser ) {
+		value = new OO.ui.HtmlSnippet( value );
+	}
 	var $cell = OOJSPlus.ui.data.column.Text.parent.prototype.renderCell.call( this, value, row );
 	$cell.addClass( 'text-cell' );
 	return $cell;
