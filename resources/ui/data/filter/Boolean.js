4b6feefa42db780041ca4ddac040a4bb1b549778
d.savuljesku <d.savuljesku@gmail.com>
2024-10-16 12:45:43 +0200

    Make boolean grid filter more accessible
    [4.5.x]
    
    ERM39068
    
    Change-Id: I949e33263ecf1786dcc376b8d33d2035b393a73e
    (cherry picked from commit 5fab8df968f7e7fe781914faed234c3a3d2c2748)
    (cherry picked from commit d65a30833c85d743660d9031367706462be4b175)

--- resources/ui/data/filter/Boolean.js
+++ resources/ui/data/filter/Boolean.js
@@ -1,32 +1,44 @@
 OOJSPlus.ui.data.filter.Boolean = function ( cfg ) {
 	this.value = this.getFilterValue();
 	cfg.closePopupOnChange = true;
+	this.trueLabel = cfg.trueLabel || mw.message( 'oojsplus-data-grid-filter-boolean-true' ).text();
+	this.falseLabel = cfg.falseLabel || mw.message( 'oojsplus-data-grid-filter-boolean-false' ).text();
 	OOJSPlus.ui.data.filter.Boolean.parent.call( this, cfg );
 };
 
 OO.inheritClass( OOJSPlus.ui.data.filter.Boolean, OOJSPlus.ui.data.filter.Filter );
 
 OOJSPlus.ui.data.filter.Boolean.prototype.getLayout = function() {
-	this.valueWidget = new OO.ui.ToggleSwitchWidget();
-	this.valueWidget.connect( this, {
-		change: 'changeValue'
-	} );
-
-	return new OO.ui.FieldsetLayout( { items: [
-			new OO.ui.FieldLayout( new OO.ui.LabelWidget( {
-				label: mw.message( 'oojsplus-data-grid-filter-label' ).text()
-			} ) ),
-			new OO.ui.FieldLayout( this.valueWidget, {
-				label: mw.message( 'oojsplus-data-grid-filter-boolean' ).text(),
-				align: 'left'
+	this.valueInput = new OO.ui.RadioSelectWidget( {
+		items: [
+			new OO.ui.RadioOptionWidget( {
+				data: true,
+				label: this.trueLabel
+			} ),
+			new OO.ui.RadioOptionWidget( {
+				data: false,
+				label: this.falseLabel
 			} )
 		]
+	} );
+	this.valueInput.connect( this, {
+		select: 'changeValue'
+	} );
+
+	return new OO.ui.FieldLayout( this.valueInput, {
+		label: mw.message( 'oojsplus-data-grid-filter-label' ).text(),
+		align: 'top'
 	} );
 };
 
 OOJSPlus.ui.data.filter.Boolean.prototype.getFilterValue = function() {
+	var item = this.valueInput ? this.valueInput.findSelectedItem() : false;
+	var val = false;
+	if ( item ) {
+		val = item.getData();
+	}
 	return {
-		value: this.valueWidget ? this.valueWidget.getValue() : false,
+		value: val,
 		operator: 'eq',
 		type: 'boolean'
 	};
@@ -34,15 +46,37 @@ OOJSPlus.ui.data.filter.Boolean.prototype.getFilterValue = function() {
 
 OOJSPlus.ui.data.filter.Boolean.prototype.setValue = function( value ) {
 	OOJSPlus.ui.data.filter.Boolean.parent.prototype.setValue.call( this, value );
-	this.valueWidget.setValue( value.value );
+	if ( this.valueInput && ( value === true || value === false ) ) {
+		var item = this.valueInput.selectItemByData( value );
+	}
+
 };
 
 OOJSPlus.ui.data.filter.Boolean.prototype.clearValues = function() {
-	this.valueWidget.setValue( false );
+	var selected = this.valueInput.findSelectedItem();
+	if ( selected ) {
+		this.valueInput.unselectItem( selected );
+	}
 };
 
 OOJSPlus.ui.data.filter.Boolean.prototype.matches = function( value ) {
 	return value === this.value.value;
+};
+
+OOJSPlus.ui.data.filter.Boolean.prototype.changeValue = function( value ) {
+	var value = value ? value.getData() : null;
+	var shouldClosePopup = this.closePopupOnChange;
+	if ( value === null ) {
+		this.value = null;
+		this.clearButton.setDisabled( true );
+		shouldClosePopup = true;
+	} else {
+		this.conditionValue = value;
+		this.value = this.getFilterValue();
+		this.clearButton.setDisabled( false );
+	}
+
+	this.emit( 'change', this, shouldClosePopup );
 };
 
 OOJSPlus.ui.data.registry.filterRegistry.register( 'boolean', OOJSPlus.ui.data.filter.Boolean );
