daae18ff2225deaa9b79e1b5945d13bf33f5ab96
d.savuljesku <d.savuljesku@gmail.com>
2024-12-10 16:22:47 +0100

    Add ContributionWidget
    This widget is similar to UserWidget but also shows a timestamp of certain event
    To be used in stuff like comments
    
    [5.0]
    
    ERM39792
    
    Change-Id: I5aecf0622433f5d60d695f23709ab33660f88233

---
+++ resources/ui/widget/ContributionWidget.js
@@ -0,0 +1,33 @@
+OOJSPlus.ui.widget.ContributionWidget = function( timestamp, cfg ) {
+	this.timestamp = timestamp;
+
+	OOJSPlus.ui.widget.ContributionWidget.parent.call( this, cfg );
+	this.$element.addClass( 'oojsplus-contribution-widget' );
+};
+
+OO.inheritClass( OOJSPlus.ui.widget.ContributionWidget, OOJSPlus.ui.widget.UserWidget );
+
+OOJSPlus.ui.widget.ContributionWidget.prototype.render = function() {
+	console.log( this.user );
+	this.$nameBox.append( $( '<span>' ).addClass( 'user-display' ).text( this.getDisplayName() ) );
+	if ( this.showRawUsername && this.getDisplayName() !== this.user.user_name ) {
+		this.$nameBox.append( $( '<span>' ).addClass( 'user-username' ).text( this.user.user_name ) );
+	}
+
+	if ( this.showImage ) {
+		var $userImage = $( '<span>' ).addClass( 'user-image' );
+		if ( this.user.hasOwnProperty( 'user_image' ) && this.user.user_image ) {
+			$userImage.html( this.user.user_image );
+		} else {
+			$userImage.append( $( '<div>' ).addClass( 'user-image-default' ) );
+		}
+		this.$element.append( $userImage );
+	}
+	if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
+		var $link = $( '<a>' ).attr( 'href', this.user.page_url );
+		this.$nameBox.find( '.user-display' ).wrap( $link );
+	}
+	this.$nameBox.append( $( '<span>' ).addClass( 'action-timestamp' ).text( this.timestamp ) );
+	this.$element.append( this.$nameBox );
+	this.emit( 'loaded' );
+};
