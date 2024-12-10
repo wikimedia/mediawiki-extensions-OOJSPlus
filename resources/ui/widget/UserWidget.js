f42b1f7cb92b8c3af4d30c00649bce3ec2804e89
d.savuljesku <d.savuljesku@gmail.com>
2024-12-10 16:22:47 +0100

    Add ContributionWidget
    This widget is similar to UserWidget but also shows a timestamp of certain event
    To be used in stuff like comments
    
    [5.0]
    
    ERM39792
    
    Change-Id: I5aecf0622433f5d60d695f23709ab33660f88233

--- resources/ui/widget/UserWidget.js
+++ resources/ui/widget/UserWidget.js
@@ -4,51 +4,14 @@ OOJSPlus.ui.widget.UserWidget = function( cfg ) {
 	OOJSPlus.ui.widget.UserWidget.parent.call( this, cfg );
 	OO.ui.mixin.PendingElement.call( this, {} );
 
-	this.showRawUsername = cfg.showRawUsername === undefined ? true : cfg.showRawUsername;
+	this.showRawUsername = cfg.showRawUsername || false;
 	this.showImage = cfg.showImage === undefined ? true : cfg.showImage;
 	this.showLink = cfg.showLink === undefined ? true : cfg.showLink;
 
 	this.$element.addClass( 'oojsplus-user-widget' );
 	this.$nameBox = $( '<div>' ).addClass( 'user-name-cnt' );
 	this.assertUserData().done( function() {
-		this.$nameBox.append( $( '<span>' ).addClass( 'user-display' ).text( this.getDisplayName() ) );
-		if ( this.showRawUsername && this.getDisplayName() !== this.user.user_name ) {
-			this.$nameBox.append( $( '<span>' ).addClass( 'user-username' ).text( this.user.user_name ) );
-		}
-
-		if ( this.showImage ) {
-			var $userImage = $( '<span>' ).addClass( 'user-image' );
-			if ( this.user.hasOwnProperty( 'user_image' ) && this.user.user_image ) {
-				$userImage.html( this.user.user_image );
-				if ( this.showLink ) {
-					var $anchor = $userImage.find( 'a' ).first();
-					$anchor.append( this.$nameBox );
-					this.$element.append( $userImage );
-				} else {
-					this.$element.append( $userImage );
-					this.$element.append( this.$nameBox );
-				}
-			} else {
-				var $defaultImage = $( '<div>' ).addClass( 'user-image-default' );
-				if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
-					var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
-					$defaultImage.append( $link );
-					$userImage.append( $defaultImage );
-					this.$element.append( $userImage );
-				} else {
-					$userImage.append( $defaultImage );
-					this.$element.append( $userImage );
-					this.$element.append( this.$nameBox );
-				}
-			}
-		} else if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
-			var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
-			this.$element.html( $link );
-		} else {
-			this.$element.append( this.$nameBox );
-		}
-		this.emit( 'loaded' );
-
+		this.render();
 	}.bind( this ) );
 };
 
@@ -56,6 +19,46 @@ OO.inheritClass( OOJSPlus.ui.widget.UserWidget, OO.ui.Widget );
 OO.mixinClass( OOJSPlus.ui.widget.UserWidget, OO.ui.mixin.PendingElement );
 
 OOJSPlus.ui.widget.UserWidget.static.tagName = 'div';
+
+OOJSPlus.ui.widget.UserWidget.prototype.render = function() {
+	this.$nameBox.append( $( '<span>' ).addClass( 'user-display' ).text( this.getDisplayName() ) );
+	if ( this.showRawUsername && this.getDisplayName() !== this.user.user_name ) {
+		this.$nameBox.append( $( '<span>' ).addClass( 'user-username' ).text( this.user.user_name ) );
+	}
+
+	if ( this.showImage ) {
+		var $userImage = $( '<span>' ).addClass( 'user-image' );
+		if ( this.user.hasOwnProperty( 'user_image' ) && this.user.user_image ) {
+			$userImage.html( this.user.user_image );
+			if ( this.showLink ) {
+				var $anchor = $userImage.find( 'a' ).first();
+				$anchor.append( this.$nameBox );
+				this.$element.append( $userImage );
+			} else {
+				this.$element.append( $userImage );
+				this.$element.append( this.$nameBox );
+			}
+		} else {
+			var $defaultImage = $( '<div>' ).addClass( 'user-image-default' );
+			if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
+				var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
+				$defaultImage.append( $link );
+				$userImage.append( $defaultImage );
+				this.$element.append( $userImage );
+			} else {
+				$userImage.append( $defaultImage );
+				this.$element.append( $userImage );
+				this.$element.append( this.$nameBox );
+			}
+		}
+	} else if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
+		var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
+		this.$element.html( $link );
+	} else {
+		this.$element.append( this.$nameBox );
+	}
+	this.emit( 'loaded' );
+};
 
 OOJSPlus.ui.widget.UserWidget.prototype.getDisplayName = function () {
 	return this.user.user_real_name || this.user.user_name;
