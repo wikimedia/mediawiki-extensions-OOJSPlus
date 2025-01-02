c59ca36a8dc8657863de55f0a7df6e758c1e5a0c
d.savuljesku <d.savuljesku@gmail.com>
2025-01-02 08:34:31 +0100

    Add "HeadingLabel" widget, that adds an H2 heading, for WCAG
    [5.0]
    
    ERM39446
    
    Change-Id: Ideb2b50b66c841a80865978e79c7d4f446e1c4cf

---
+++ resources/ui/widget/HeadingLabel.js
@@ -0,0 +1,16 @@
+OOJSPlus.ui.widget.HeadingLabel = function( cfg ) {
+	cfg = cfg || {};
+
+	OOJSPlus.ui.widget.HeadingLabel.parent.call( this, cfg );
+	this.$element.append( $( '<h2>' ).text( cfg.label || '' ) );
+	if ( cfg.subtitle ) {
+		this.$element.append( new OO.ui.LabelWidget( {
+			label: cfg.subtitle,
+			classes: [ 'oojsplus-ui-widget-heading-label-subtitle' ]
+		} ).$element );
+	}
+
+	this.$element.addClass( 'oojsplus-ui-widget-heading-label' );
+};
+
+OO.inheritClass( OOJSPlus.ui.widget.HeadingLabel, OO.ui.Widget );
