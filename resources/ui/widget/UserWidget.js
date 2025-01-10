7bc5c26fc15828f35cf8233c7c1fa01501152403
d.savuljesku <d.savuljesku@gmail.com>
2025-01-10 13:48:12 +0100

    Integrate UserInfo popup to user widget
    [5.0]
    
    ERM39456
    
    Change-Id: I4e0a97dc514f531ee52fa730d582ca3d6bd58361

--- resources/ui/widget/UserWidget.js
+++ resources/ui/widget/UserWidget.js
@@ -7,6 +7,7 @@ OOJSPlus.ui.widget.UserWidget = function( cfg ) {
 	this.showRawUsername = cfg.showRawUsername || false;
 	this.showImage = cfg.showImage === undefined ? true : cfg.showImage;
 	this.showLink = cfg.showLink === undefined ? true : cfg.showLink;
+	this.showProfileOnHover = typeof cfg.showProfileOnHover === 'undefined' ? true : cfg.showProfileOnHover;
 
 	this.$element.addClass( 'oojsplus-user-widget' );
 	this.$nameBox = $( '<div>' ).addClass( 'user-name-cnt' );
@@ -25,14 +26,14 @@ OOJSPlus.ui.widget.UserWidget.prototype.render = function() {
 	if ( this.showRawUsername && this.getDisplayName() !== this.user.user_name ) {
 		this.$nameBox.append( $( '<span>' ).addClass( 'user-username' ).text( this.user.user_name ) );
 	}
-
+	let $link = null;
 	if ( this.showImage ) {
 		var $userImage = $( '<span>' ).addClass( 'user-image' );
 		if ( this.user.hasOwnProperty( 'user_image' ) && this.user.user_image ) {
 			$userImage.html( this.user.user_image );
 			if ( this.showLink ) {
-				var $anchor = $userImage.find( 'a' ).first();
-				$anchor.append( this.$nameBox );
+				$link = $userImage.find( 'a' ).first();
+				$link.append( this.$nameBox );
 				this.$element.append( $userImage );
 			} else {
 				this.$element.append( $userImage );
@@ -41,7 +42,7 @@ OOJSPlus.ui.widget.UserWidget.prototype.render = function() {
 		} else {
 			var $defaultImage = $( '<div>' ).addClass( 'user-image-default' );
 			if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
-				var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
+				$link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
 				$defaultImage.append( $link );
 				$userImage.append( $defaultImage );
 				this.$element.append( $userImage );
@@ -52,10 +53,24 @@ OOJSPlus.ui.widget.UserWidget.prototype.render = function() {
 			}
 		}
 	} else if ( this.showLink && this.user.hasOwnProperty( 'page_url' ) ) {
-		var $link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
+		$link = $( '<a>' ).attr( 'href', this.user.page_url ).append( this.$nameBox );
 		this.$element.html( $link );
 	} else {
 		this.$element.append( this.$nameBox );
+	}
+
+	if (
+		$link &&
+		this.showProfileOnHover &&
+		ext.userProfile &&
+		typeof ext.userProfile.openUserInfoPopup === 'function'
+	) {
+		$link.on( 'mouseover', function( event ) {
+			ext.userProfile.openUserInfoPopup( this.user.user_name, $link );
+		}.bind( this ) );
+	} else if ( !this.showProfileOnHover && $link ) {
+		// Prevent automatic profile popup if not requested
+		$link.removeAttr( 'data-bs-username' );
 	}
 	this.emit( 'loaded' );
 };
