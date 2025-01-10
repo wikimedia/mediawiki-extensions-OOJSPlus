aad8dc375aab29ba6d43cd7dca159efcb8dc2482
d.savuljesku <d.savuljesku@gmail.com>
2025-01-10 11:38:01 +0100

    Add "list" tool type to ManagerToolbar
    [5.0]
    
    ERM39440
    
    Change-Id: I1654e50627548730cc32e05ee21598fc242c0d38

---
+++ resources/ui/toolbar/tool/List.js
@@ -0,0 +1,24 @@
+OOJSPlus.ui.toolbar.tool.List = function ( cfg ) {
+	cfg = cfg || {};
+
+	this.type = 'list';
+	this.indicator = cfg.indicator || '';
+	this.icon = cfg.icon || '';
+	this.label = cfg.label || '';
+	this.include = cfg.include || [];
+	this.position = cfg.position || 'left';
+	this.classes = cfg.classes || [ 'position-' + this.position ];
+};
+
+OO.initClass( OOJSPlus.ui.toolbar.tool.List );
+
+OOJSPlus.ui.toolbar.tool.List.prototype.getDefinition = function () {
+	return {
+		type: this.type,
+		indicator: this.indicator,
+		icon: this.icon,
+		label: this.label,
+		include: this.include,
+		classes: this.classes
+	};
+};
--- resources/ui/toolbar/tool/ToolbarTool.js
+++ resources/ui/toolbar/tool/ToolbarTool.js
@@ -8,7 +8,11 @@ OOJSPlus.ui.toolbar.tool.ToolbarTool = function ( cfg ) {
 	this.icon = cfg.icon || '';
 	this.flags = cfg.flags || [];
 	this.title = cfg.title || '';
+
+	this.active = cfg.active || false;
+	this.displayToggleCheckbox = cfg.displayToggleCheckbox || false;
 	this.position = cfg.position || 'left';
+
 	this.displayBothIconAndLabel = cfg.displayBothIconAndLabel || false;
 	// Do not use callbacks unless necessary - it splits code in unpredictable ways
 	this.callback = cfg.callback || null;
@@ -24,6 +28,22 @@ OOJSPlus.ui.toolbar.tool.ToolbarTool.prototype.getToolObject = function () {
 	var self = this;
 	const tool = function() {
 		tool.super.apply( this, arguments );
+		if ( self.active ) {
+			this.setActive( true );
+		}
+		this.$element.addClass( 'oojsplus-toolbar-tool' );
+		if ( self.displayToggleCheckbox ) {
+			/*this.toggleCheckbox = new OO.ui.CheckboxInputWidget( {
+				selected: self.active,
+				classes: [ 'oojsplus-toolbar-tool-checkbox' ]
+			} );*/
+			this.toggleCheckbox = new OO.ui.IconWidget( {
+				icon: self.active ? 'check' : '',
+				classes: [ 'oojsplus-toolbar-tool-checkbox' ],
+				flags: [ 'progressive' ]
+			} );
+			this.$link.prepend( this.toggleCheckbox.$element );
+		}
 	};
 	OO.inheritClass( tool, OO.ui.Tool );
 	tool.static.name = this.name;
@@ -38,6 +58,12 @@ OOJSPlus.ui.toolbar.tool.ToolbarTool.prototype.getToolObject = function () {
 			this.setActive( false );
 		}
 		self.emit( 'action', self.name );
+	};
+	tool.prototype.setActive = function( state ) {
+		if ( this.toggleCheckbox ) {
+			this.toggleCheckbox.setIcon( state ? 'check' : '' );
+		}
+		tool.super.prototype.setActive.call( this, state );
 	};
 	tool.prototype.onUpdateState = typeof this.onUpdateState === 'function' ?
 		this.onUpdateState.bind( this ) :
