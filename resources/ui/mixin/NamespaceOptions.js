cfc1d1c6bff3d3211244a467d15b53a275539925
d.savuljesku <d.savuljesku@gmail.com>
2025-01-13 11:32:49 +0100

    Add NamespaceMultiselectWidget
    [4.5.x]
    
    ERM39444
    
    Change-Id: I0f8715f20b4bfd1484cbcff725c029dcc3ff94a4
    (cherry picked from commit 3b2ee7ad58e4b3577c218a430c4400cc7165d893)

---
+++ resources/ui/mixin/NamespaceOptions.js
@@ -0,0 +1,98 @@
+OOJSPlus.ui.mixin.NamespaceOptions = function() {};
+
+OO.initClass( OOJSPlus.ui.mixin.NamespaceOptions );
+
+OOJSPlus.ui.mixin.NamespaceOptions.prototype.getNamespaceOptions = function ( config ) {
+	var i = 0, name = '',
+		namespaces = mw.config.get( 'wgFormattedNamespaces' ),
+		options = [],
+		content = mw.config.get( 'wgContentNamespaces' ),
+		exclude = config.exclude || [],
+		groups = {
+			content: {
+				label: mw.msg( 'oojsplus-namespace-content' ),
+				options: []
+			},
+			system: {
+				label: mw.msg( 'oojsplus-namespace-system' ),
+				options: []
+			},
+			content_talk: {
+				label: mw.msg( 'oojsplus-namespace-content-talk' ),
+				options: []
+			},
+			system_talk: {
+				label: mw.msg( 'oojsplus-namespace-system-talk' ),
+				options: []
+			}
+		};
+
+	for ( var ns in namespaces ) {
+		if ( !namespaces.hasOwnProperty( ns ) ) {
+			continue;
+		}
+		ns = Number( ns );
+		if ( ns < 0 || exclude.indexOf( ns ) !== -1 ) {
+			continue;
+		}
+		name = namespaces[ ns ];
+		if ( ns === 0 ) {
+			name = mw.msg( 'blanknamespace' );
+		}
+
+		if ( content.indexOf( ns ) !== -1 || content.indexOf( ns - 1 ) !== -1 ) {
+			if ( ns % 2 === 0 ) {
+				groups.content.options.push( { data: ns, label: name } );
+			} else {
+				groups.content_talk.options.push( { data: ns, label: name } );
+			}
+		} else {
+			if ( ns % 2 === 0 ) {
+				groups.system.options.push( { data: ns, label: name } );
+			} else {
+				groups.system_talk.options.push( { data: ns, label: name } );
+			}
+		}
+	}
+
+	if ( config.specialOptionAll ) {
+		options.push( new OO.ui.MenuOptionWidget( {
+			data: -99,
+			label: mw.msg( 'namespacesall' )
+		} ) );
+	}
+	for ( var group in groups ) {
+		if ( !groups.hasOwnProperty( group ) ) {
+			continue;
+		}
+		if ( config.groups && config.groups.indexOf( group ) === -1 ) {
+			continue;
+		}
+		// Sort options
+		groups[ group ].options.sort( function ( a, b ) {
+			return a.label - b.label;
+		} );
+		if ( groups[ group ].options.length ) {
+			if ( !config.hideHeadings ) {
+				options.push( new OO.ui.MenuSectionOptionWidget( {
+					label: groups[ group ].label
+				} ) );
+			}
+			for ( i = 0; i < groups[ group ].options.length; i++ ) {
+				options.push( new OO.ui.MenuOptionWidget( {
+					data: groups[ group ].options[ i ].data,
+					label: groups[ group ].options[ i ].label
+				} ) );
+			}
+		}
+	}
+
+	if ( config.includeAllValue !== null && config.includeAllValue !== undefined ) {
+		options.unshift( {
+			data: config.includeAllValue,
+			label: mw.msg( 'namespacesall' )
+		} );
+	}
+
+	return options;
+};
--- resources/ui/widget/NamespaceInputWidget.js
+++ resources/ui/widget/NamespaceInputWidget.js
@@ -1,10 +1,10 @@
 OOJSPlus.ui.widget.NamespaceInputWidget = function( config ) {
+	OOJSPlus.ui.mixin.NamespaceOptions.call( this );
 	config = config || {};
 	config.$overlay = config.$overlay || true;
 	config.menu = {
-		items: OOJSPlus.ui.widget.NamespaceInputWidget.static.getNamespaceDropdownOptions( config )
+		items: this.getNamespaceOptions( config )
 	};
-
 	OOJSPlus.ui.widget.NamespaceInputWidget.parent.call( this, config );
 
 	var selectable = this.getMenu().findFirstSelectableItem();
@@ -14,90 +14,8 @@ OOJSPlus.ui.widget.NamespaceInputWidget = function( config ) {
 	this.$element.addClass( 'oojsplus-widget-namespaceInputWidget' );
 };
 
-/* Setup */
-
 OO.inheritClass( OOJSPlus.ui.widget.NamespaceInputWidget, OO.ui.DropdownWidget );
-
-OOJSPlus.ui.widget.NamespaceInputWidget.static.getNamespaceDropdownOptions = function ( config ) {
-	var i = 0, name = '',
-		namespaces = mw.config.get( 'wgFormattedNamespaces' ),
-		options = [],
-		content = mw.config.get( 'wgContentNamespaces' ),
-		exclude = config.exclude || [],
-		groups = {
-			content: {
-				label: mw.msg( 'oojsplus-namespace-content' ),
-				options: []
-			},
-			system: {
-				label: mw.msg( 'oojsplus-namespace-system' ),
-				options: []
-			},
-			content_talk: {
-				label: mw.msg( 'oojsplus-namespace-content-talk' ),
-				options: []
-			},
-			system_talk: {
-				label: mw.msg( 'oojsplus-namespace-system-talk' ),
-				options: []
-			}
-		};
-
-	for ( var ns in namespaces ) {
-		if ( !namespaces.hasOwnProperty( ns ) ) {
-			continue;
-		}
-		ns = Number( ns );
-		if ( ns < 0 || exclude.indexOf( ns ) !== -1 ) {
-			continue;
-		}
-		name = namespaces[ ns ];
-		if ( ns === 0 ) {
-			name = mw.msg( 'blanknamespace' );
-		}
-
-		if ( content.indexOf( ns ) !== -1 || content.indexOf( ns - 1 ) !== -1 ) {
-			if ( ns % 2 === 0 ) {
-				groups.content.options.push( { data: ns, label: name } );
-			} else {
-				groups.content_talk.options.push( { data: ns, label: name } );
-			}
-		} else {
-			if ( ns % 2 === 0 ) {
-				groups.system.options.push( { data: ns, label: name } );
-			} else {
-				groups.system_talk.options.push( { data: ns, label: name } );
-			}
-		}
-	}
-
-	for ( var group in groups ) {
-		// Sort options
-		groups[ group ].options.sort( function ( a, b ) {
-			return a.label - b.label;
-		} );
-		if ( groups[ group ].options.length ) {
-			options.push( new OO.ui.MenuSectionOptionWidget( {
-				label: groups[ group ].label
-			} ) );
-			for ( i = 0; i < groups[ group ].options.length; i++ ) {
-				options.push( new OO.ui.MenuOptionWidget( {
-					data: groups[ group ].options[ i ].data,
-					label: groups[ group ].options[ i ].label
-				} ) );
-			}
-		}
-	}
-
-	if ( config.includeAllValue !== null && config.includeAllValue !== undefined ) {
-		options.unshift( {
-			data: config.includeAllValue,
-			label: mw.msg( 'namespacesall' )
-		} );
-	}
-
-	return options;
-};
+OO.mixinClass( OOJSPlus.ui.widget.NamespaceInputWidget, OOJSPlus.ui.mixin.NamespaceOptions );
 
 OOJSPlus.ui.widget.NamespaceInputWidget.prototype.setValue = function( value ) {
 	if ( !value ) {
