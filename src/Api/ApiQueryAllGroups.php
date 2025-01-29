259d1d15e55071b2fc4a931c31e24a5c0f88d36b
hslater <hamish.slater@hallowelt.com>
2025-01-29 09:46:09 +0100

    Update ApiQueryBase namespace
    ERM38444
    
    [REL1_43, master]
    
    Change-Id: Ibe06a94faa9ecd3145cc2d766470500972d989fb

--- src/Api/ApiQueryAllGroups.php
+++ src/Api/ApiQueryAllGroups.php
@@ -2,10 +2,10 @@
 
 namespace OOJSPlus\Api;
 
-use ApiQueryBase;
 use ApiQueryBlockInfoTrait;
 use MediaWiki\Api\ApiBase;
 use MediaWiki\Api\ApiQuery;
+use MediaWiki\Api\ApiQueryBase;
 use MWStake\MediaWiki\Component\Utils\Utility\GroupHelper;
 use MWStake\MediaWiki\Component\Utils\UtilityFactory;
 use Wikimedia\ParamValidator\ParamValidator;
