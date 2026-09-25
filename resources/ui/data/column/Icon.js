743bf337330290e8a707c533a26423d15052077d
Mschlindwein <schlindwein@hallowelt.com>
2026-09-25 15:31:03 +0200

    Enable aria-label for icon column
    ERM49376
    
    [5.1.x]
    
    Change-Id: I811f56d14f068beb5d4d4a883485bc94de8f9345
    (cherry picked from commit 74e8230b2f599670a790e6e47989d382b64c417e)
    (cherry picked from commit 02c2641ca7b457201bc3f484d49d5eea2dcfa4ea)

--- resources/ui/data/column/Icon.js
+++ resources/ui/data/column/Icon.js
@@ -1,6 +1,8 @@
 OOJSPlus.ui.data.column.Icon = function ( cfg ) {
 	OOJSPlus.ui.data.column.Icon.parent.call( this, cfg );
 
+	this.ariaLabel = cfg.ariaLabel || null;
+	this.title = cfg.title || null;
 	this.$element.addClass( 'icon-column' );
 };
 
@@ -13,10 +15,19 @@ OOJSPlus.ui.data.column.Icon.prototype.renderCell = function ( value, row ) {
 	return $cell;
 };
 
-OOJSPlus.ui.data.column.Icon.prototype.getViewControls = function ( value ) {
-	return new OO.ui.IconWidget( {
+OOJSPlus.ui.data.column.Icon.prototype.getViewControls = function ( value, row ) {
+	const widget = new OO.ui.IconWidget( {
 		icon: value
 	} );
+	if ( this.ariaLabel ) {
+		const label = typeof this.ariaLabel === 'function' ? this.ariaLabel( value, row ) : this.ariaLabel;
+		widget.$element.attr( 'aria-label', label );
+	}
+	if ( this.title ) {
+		const titleValue = typeof this.title === 'function' ? this.ariaLabel( value, row ) : this.title;
+		widget.$element.attr( 'title', titleValue );
+	}
+	return widget;
 };
 
 OOJSPlus.ui.data.registry.columnRegistry.register( 'icon', OOJSPlus.ui.data.column.Icon );
